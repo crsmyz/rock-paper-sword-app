945cd43833ee69e5851e6d6f3a4932e131345c5a
crsmyz <crsmyz@gmail.com>
2019-03-20 14:15:52 -0400

    Updates

---
+++ precache-manifest.35a7305fd31e261bbdc8f454a33bc50e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e4a48b306dfa9431278d",
+    "url": "/rock-paper-sword-app/static/css/main.30659ae3.chunk.css"
+  },
+  {
+    "revision": "e4a48b306dfa9431278d",
+    "url": "/rock-paper-sword-app/static/js/main.e4a48b30.chunk.js"
+  },
+  {
+    "revision": "ef4b73d4d8314becd315",
+    "url": "/rock-paper-sword-app/static/css/1.9d804b50.chunk.css"
+  },
+  {
+    "revision": "ef4b73d4d8314becd315",
+    "url": "/rock-paper-sword-app/static/js/1.ef4b73d4.chunk.js"
+  },
+  {
+    "revision": "72e47a2d5f3b29a0934a",
+    "url": "/rock-paper-sword-app/static/js/runtime~main.72e47a2d.js"
+  },
+  {
+    "revision": "f4b392b8e54147c65c0e4e8a9a825c80",
+    "url": "/rock-paper-sword-app/index.html"
+  }
+];
